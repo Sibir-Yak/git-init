dfcd62d7a78b381556709e53c01f74e887fb60a7
Sibir-Yak <kaplenkoyuriy@gmail.com>
2022-07-20 11:26:09 +0300

    Initial commit

--- ideal_weight.rb
+++ ideal_weight.rb
@@ -4,7 +4,13 @@ name = gets.chomp
 puts "Дабы занать вес свой идеальный  укажи рост свой реальный?"
 weight = gets.chomp
 
-sum = (weight.to_i-110) * 1.15 
+sum = (weight.to_i-110) * 1.15
+
+if weight.to_i < 140
+    puts "бро формула конченая и считает очень условно введи рост от 140"
+    exit
+end
+
 if sum > 0
     puts "#{name}, витаю тебя дуже! Твой идеальный вес #{sum.to_i} килограмчиков."
 else sum < 0
